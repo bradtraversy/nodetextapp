2908c60c7ed4aa3d8942151948a55ca91d3f29a5
Chau Nguyen <nguyentietngocchau@gmail.com>
2018-10-02 11:49:11 +0700

    refactor code, add feature schedule time send message

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 const numberInput = document.getElementById('number'),
       textInput = document.getElementById('msg'),
+      scheduleSelect = document.getElementById('schedule'),
       button = document.getElementById('button'),
       response = document.querySelector('.response');
 
@@ -12,24 +13,37 @@ socket.on('smsStatus', function(data){
   }else{
     response.innerHTML = '<h5>Text message sent to ' + data.number + '</h5>';
   }
-})
+});
 
-function send() {
+let timeOut;
+const getTimeSchedule = ({ time, number, text }) => {
+  if(timeOut) clearTimeout(timeOut);
+  timeOut = setTimeout(() => {
+    fetchServer({ number, text });
+  }, time * 60 * 1000);
+};
+
+const fetchServer = ({ number, text }) => {
   console.log('send');
-  const number = numberInput.value.replace(/\D/g, '');
-  const text = textInput.value;
-
   fetch('/', {
     method: 'post',
     headers: {
       'Content-type': 'application/json'
     },
-    body: JSON.stringify({number: number, text: text})
+    body: JSON.stringify({ number, text })
   })
-  .then(function(res){
-    console.log(res);
-  })
-  .catch(function(err){
-    console.log(err);
-  });
+    .then(function (res) {
+      console.log(res);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+};
+
+function send() {
+  const number = numberInput.value.replace(/\D/g, '');
+  const text = textInput.value;
+  const time = parseInt(scheduleSelect.value, 10);
+  getTimeSchedule({ number, text, time });
 }
+
